b9badc0c951704b4bcfaefbf8df043637d125ff1
Arianna Grangetto <ariigrangetto5@gmail.com>
2025-12-13 11:53:05 -0300

    fix: components logic

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -4,14 +4,26 @@ import getData from "../Service/getData.js";
 import { Helmet } from "react-helmet";
 import ListOfProducts from "../Components/ListOfProducst.tsx";
 import WithoutProducts from "../Components/WithoutProducts.tsx";
+import useAuth from "../hooks/useAuth.tsx";
+import { Link, useLocation } from "react-router";
 
 export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
+  const path = useLocation();
+  const [loading, setLoading] = useState<boolean>(false);
+  const { logout, login, isAuthenticated } = useAuth();
 
   useEffect(() => {
+    setLoading(true);
     async function FetchData() {
-      const data = await getData();
-      setProducts(data.products);
+      try {
+        const data = await getData();
+        setProducts(data.products);
+      } catch (error) {
+        throw new Error("Error fetching products");
+      } finally {
+        setLoading(false);
+      }
     }
 
     FetchData();
@@ -23,7 +35,13 @@ export default function Products() {
         <title>NeoMall products</title>
       </Helmet>
 
-      {products.length !== 0 ? <ListOfProducts /> : <WithoutProducts />}
+      {loading ? (
+        <h1>Cargando productos...</h1>
+      ) : products.length > 0 ? (
+        <ListOfProducts />
+      ) : (
+        <WithoutProducts />
+      )}
     </>
   );
 }
