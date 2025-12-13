a71d39f3a620880ca6d9e638a336b1fc8349fcfb
Arianna Grangetto <ariigrangetto5@gmail.com>
2025-12-13 11:50:12 -0300

    fix: components name

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
-import CartProvider from "./Context/CartReducerActions.tsx";
+import CartProvider from "./context/CartReducerActions.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <CartProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </CartProvider>
   </BrowserRouter>
 );
