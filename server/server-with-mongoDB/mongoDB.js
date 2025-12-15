a8469466fe921a69ba21b5793ec8cec2b006ab93
Arianna <ariigrangetto5@gmail.com>
2025-12-15 20:18:06 -0300

    feat: change import

--- server/server-with-mongoDB/mongoDB.js
+++ server/server-with-mongoDB/mongoDB.js
@@ -1,31 +1,31 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const connectionString = process.env.MONGODB;
 
-if(!connectionString){
-    console.error("Invalid conection string");
-    process.exit(1);
+if (!connectionString) {
+  console.error("Invalid conection string");
+  process.exit(1);
 }
 
-export async function connectDB(){
-    try{
-        await mongoose.connect(connectionString);
-        console.log("database connected");
-    }catch(e){
-        throw new Error("Error conecting database: " + e.message);
-    }
+export async function connectDB() {
+  try {
+    await mongoose.connect(connectionString);
+    console.log("database connected");
+  } catch (e) {
+    throw new Error("Error conecting database: " + e.message);
+  }
 }
 
-process.on("SIGINT", async() => {
-    await mongoose.connection.close();
-    console.log("conection closed");
-    process.exit(1);
-})
+process.on("SIGINT", async () => {
+  await mongoose.connection.close();
+  console.log("conection closed");
+  process.exit(1);
+});
 
 //in case of error
 
 process.on("uncaughtException", (error) => {
-    console.log(error);
-    mongoose.disconnect();
-})
+  console.log(error);
+  mongoose.disconnect();
+});
