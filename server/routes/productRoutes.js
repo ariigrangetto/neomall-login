bbad1ea468d2ea4a4c86d0bab005b5bdf07fbdf0
Arianna <ariigrangetto5@gmail.com>
2025-12-18 21:04:54 -0300

    feat: new products routes

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import {
+  filterProducts,
   getAllProducts,
   getProductById,
 } from "../controller/productController.js";
 
 const productsRoutes = Router();
 
+productsRoutes.get("/", filterProducts);
 productsRoutes.get("/", getAllProducts);
 productsRoutes.get("/:id", getProductById);
 
