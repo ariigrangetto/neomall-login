5c27ad677a46998172feb97da72aa5bcf42aa48b
Arianna <ariigrangetto5@gmail.com>
2025-12-19 12:24:18 -0300

    feat: cart fetchs

---
+++ client/src/api/cart.js
@@ -0,0 +1,17 @@
+const API = import.meta.env.API;
+
+export default async function getCart() {
+  try {
+    const response = await fetch(`${API}/cart`, {
+      method: "GET",
+      header: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    const data = await response.json();
+    return { status: response.status, data: data };
+  } catch (error) {
+    throw new Error("Error fething data");
+  }
+}
