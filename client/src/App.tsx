c26e5db0edc0fe2d6dd99dfb2e588a990c9bd6b5
Arianna <ariigrangetto5@gmail.com>
2025-12-19 11:55:50 -0300

    feat: remove header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router";
 import Header from "./components/Header.tsx";
 import Loading from "./pages/Loading.tsx";
 import Home from "./pages/Home.tsx";
-import Footer from "./components/Footer.tsx";
 import { lazy, Suspense } from "react";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
@@ -17,7 +16,6 @@ const Products = lazy(() => import("./pages/Products.tsx"));
 function App() {
   return (
     <>
-      <Header />
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path='/' element={<Home />} />
