32d9c8f7fb003316b6ef84cb48e1bdfdabe839ba
Arianna <ariigrangetto5@gmail.com>
2026-01-05 20:09:58 -0300

    feat: deleting reducer

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
-import CartProvider from "./context/CartReducerActions.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <CartProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </CartProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 );
