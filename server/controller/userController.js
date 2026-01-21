33ddd5fa2c83737f0826459dc4f91cb6420ddf21
Arianna <ariigrangetto5@gmail.com>
2026-01-21 20:17:51 -0300

    feat: separate logic

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,9 +1,9 @@
-import { connection } from "../model/mysql/connection.js";
+import { connection } from "../config/mysql/mysqlConnection.js";
+import { UserModel } from "../model/useModel.js";
 import {
   validateLoginSchema,
   validateRegisterSchema,
 } from "../schema/userSchema.js";
-import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const createAccessToken = (payload) => {
@@ -24,34 +24,20 @@ export const register = async (req, res) => {
   const { email, username, password } = result.data;
 
   try {
-    const [rows] = await connection.query(
-      `SELECT user_id FROM users WHERE email = ?`,
-      [email]
+    const { rows, userId, response } = await UserModel.register(
+      email,
+      username,
+      password,
     );
 
     if (rows.length > 0) {
       return res.status(400).json([{ message: "User already exists" }]);
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    await connection.query(
-      `INSERT INTO users (email, username, password)
-        VALUES (?, ?, ?)`,
-      [email, username, hashedPassword]
-    );
-
-    const [userCreated] = await connection.query(
-      `SELECT BIN_TO_UUID(user_id) AS userId, username, email, created_at
-      FROM users`
-    );
-
-    const userId = userCreated[0].user_id;
-
     const token = await createAccessToken({ userId });
     res.cookie("token", token);
 
-    return res.status(201).json(userCreated[0]);
+    return res.status(201).json(response);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -64,34 +50,22 @@ export const login = async (req, res) => {
   }
   try {
     const { email, password } = result.data;
-    const [rows] = await connection.query(
-      `SELECT BIN_TO_UUID(user_id) AS userId, username, email, password FROM users WHERE email = ?`,
-      [email]
+    const { isMatchPassword, rows, userId, response } = await UserModel.login(
+      email,
+      password,
     );
-
     if (rows.length < 0) {
       return res.status(400).json([{ message: "User not found" }]);
     }
-
-    const isMatchPassword = await bcrypt.compare(password, rows[0].password);
 
     if (!isMatchPassword) {
       return res.status(400).json([{ message: "Incorrect password" }]);
     }
 
-    const userId = rows[0].userId;
-
-    console.log(userId);
-
     const token = await createAccessToken({ userId });
     res.cookie("token", token);
 
-    const [response] = await connection.query(
-      `SELECT BIN_TO_UUID(user_id) AS userId, username, email, created_at
-        FROM users`
-    );
-
-    return res.status(201).json(response[0]);
+    return res.status(201).json(response);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -103,17 +77,9 @@ export const logout = async (req, res) => {
 };
 
 export const profile = async (req, res) => {
-  console.log(req.user.userId);
   const id = req.user.userId;
   try {
-    const [foundUser] = await connection.query(
-      `
-            SELECT BIN_TO_UUID(user_id) AS userId, username, email, created_at
-            FROM users
-            WHERE user_id = UUID_TO_BIN(?)
-            `,
-      [id]
-    );
+    const foundUser = await UserModel.profile(id);
 
     if (foundUser.length === 0) {
       return res.status(400).json([{ message: "user not found" }]);
@@ -136,14 +102,7 @@ export const verifyToken = async (req, res) => {
     jwt.verify(token, process.env.TOKEN_KEY, async (error, decoded) => {
       if (error) return res.status(401).json([{ message: "Unauthorized" }]);
 
-      const [foundUser] = await connection.query(
-        `
-        SELECT BIN_TO_UUID(user_id) AS userId, username, email, created_at
-        FROM users
-        WHERE user_id = UUID_TO_BIN(?)
-        `,
-        [decoded.userId]
-      );
+      const foundUser = await UserModel.verifyToken(decoded.userId);
 
       return res.json(foundUser[0]);
     });
