fe457182dc09d63e59e2984ee3fdeba50f5c46da
Arianna <ariigrangetto5@gmail.com>
2026-01-26 10:45:28 -0300

    fix: database connection

--- server/config/mysql/mysqlConnection.js
+++ server/config/mysql/mysqlConnection.js
@@ -1,14 +1,6 @@
 import mysql from "mysql2/promise";
 import "dotenv/config";
 
-console.log({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
-
 export const createDBConnection = async () => {
   return mysql.createConnection({
     host: process.env.MYSQL_HOST,
