265c492afdc51f61ed6866e2e29936701c918ee7
Arianna <ariigrangetto5@gmail.com>
2026-01-28 11:15:13 -0300

    fix: mysql port import

--- server/config/mysql/mysqlConnection.js
+++ server/config/mysql/mysqlConnection.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 export const createDBConnection = async () => {
   return mysql.createConnection({
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
